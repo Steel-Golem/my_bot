ab3ac5ef5755211bb05d73e62b2488c7a00fd4f0
Ethan Schneiderman <scheth@bu.edu>
2025-10-26 13:04:12 -0400

    trying something

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -3,75 +3,67 @@ import xacro
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-
-from launch.actions import SetEnvironmentVariable
-
-
-
 
 def generate_launch_description():
+    package_name = 'my_bot'
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='my_bot' 
-
+    # Launch robot_state_publisher
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
+        ]),
+        launch_arguments={'use_sim_time': 'true'}.items()
     )
-    
-    world = LaunchConfiguration('world')
 
+    # World argument
+    world = LaunchConfiguration('world')
     world_arg = DeclareLaunchArgument(
         'world',
-        default_value="empty.sdf",
+        default_value="empty.world",
         description='World to load'
-        )
+    )
 
-    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot',
-                                   '-z', '0.1'],
-                        output='screen')
+    # Run Gazebo Classic with the world
+    gazebo = Node(
+        package='gazebo_ros',
+        executable='gazebo',
+        arguments=['-s', 'libgazebo_ros_factory.so', LaunchConfiguration('world')],
+        output='screen'
+    )
 
-    bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
-                    arguments=[
-                        '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
-                        '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
-                        '/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock'
-                    ],
-                    output='screen')
+    # Spawn the robot into Gazebo
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot', '-x', '0', '-y', '0', '-z', '0.1'],
+        output='screen'
+    )
 
-
+    # Load robot description
     xacro_file = os.path.join(
-                    get_package_share_directory('my_bot'),
-                    'description',
-                    'robot.urdf.xacro'
-                    )
+        get_package_share_directory(package_name),
+        'description',
+        'robot.urdf.xacro'
+    )
     robot_description_config = xacro.process_file(xacro_file).toxml()
 
-    
+    # ROS2 Control node
     controller_manager_node = Node(
-        package='controller_manager', 
+        package='controller_manager',
         executable='ros2_control_node',
         parameters=[
-                    {'robot_description': robot_description_config},
-                    os.path.join(get_package_share_directory('my_bot'), 'config', 'my_controllers.yaml')
+            {'robot_description': robot_description_config},
+            os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
         ],
         output='screen'
     )
-    
+
+    # Spawn controllers with timers to ensure controller_manager is ready
     load_joint_state_broadcaster = Node(
         package='controller_manager',
         executable='spawner',
@@ -86,17 +78,12 @@ def generate_launch_description():
         output='screen'
     )
 
-    # Launch them all!
     return LaunchDescription([
-    SetEnvironmentVariable(
-        'GAZEBO_PLUGIN_PATH',
-        '/opt/ros/humble/lib'
-    ),
-    rsp,
-    world_arg,
-    spawn_entity,
-    bridge,
-    controller_manager_node,
-    TimerAction(period=5.0, actions=[load_joint_state_broadcaster]),
-    TimerAction(period=6.0, actions=[load_diff_drive_controller]),
+        rsp,
+        world_arg,
+        gazebo,
+        spawn_entity,
+        controller_manager_node,
+        TimerAction(period=2.0, actions=[load_joint_state_broadcaster]),
+        TimerAction(period=2.5, actions=[load_diff_drive_controller]),
     ])
