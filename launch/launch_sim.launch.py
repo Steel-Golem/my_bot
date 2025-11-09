2f0ee63459d649523e61c328bacd1fefe5d06f15
Ethan Schneiderman <scheth@bu.edu>
2025-11-09 10:54:09 -0500

    changing paramater names to specifiy direction

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -43,8 +43,9 @@ def generate_launch_description():
     bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
                     arguments=[
                         '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist:ros2',
-                        '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
-                        '/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock'
+                        '/odom@gz.msgs.Odometry@nav_msgs/msg/Odometry:gz',
+                        '/clock@gz.msgs.Clock@rosgraph_msgs/msg/Clock:gz'
+
                     ],
                     output='screen')
 
